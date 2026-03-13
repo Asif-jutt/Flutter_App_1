d39c4bd3afa0f8bea63f1782a0007240ace9fea3
Asif Hussain <asifhussain5115@gmail.com>
2026-03-13 17:05:41 +0500

    images feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:flutter_week3/pages/login_page.dart';
 import 'pages/home_page.dart';
+import 'pages/homepractice.dart';
 import 'pages/homepractice.dart';
 
 void main() {
@@ -11,15 +13,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Flutter App", 
-    themeMode: ThemeMode.dark,
-    theme: ThemeData(primarySwatch: Colors.purple),
-    darkTheme: ThemeData(brightness: Brightness.dark),
-    initialRoute: "/home",
-    routes: {
-      "/home":(context)=>Homepractic(),
-      "/":(context)=>HomePage(),
-    },
+    return MaterialApp(
+      title: "Flutter App",
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.purple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/login",
+      routes: {
+        "/home": (context) => Homepractic(),
+        "/": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
